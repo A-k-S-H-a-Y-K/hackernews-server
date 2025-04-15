8b713448b8fc4acfc663b470a9997063decca79d
AKSHAY K <akshay47kk@gmail.com>
2025-04-15 19:30:44 +0530

    Refactor posts controller and types for improved error handling, response structure, and consistency in user data representation

--- src/controllers/posts/posts-types.ts
+++ src/controllers/posts/posts-types.ts
@@ -1,13 +1,19 @@
-import type { Post} from "@prisma/client";
+import type { Post, User } from "@prisma/client";
 
-export type GetPostsResult = {
-  posts: Post[];
+export type GetAllPostsResult = {
+  posts: (Post & { user: Pick<User, "username" | "name"> })[];
+};
+
+export type GetMyPostsResult = {
+  posts: (Post & { user: Pick<User, "username" | "name"> })[];
 };
 
 export type CreatePostResult = {
-  newPost: Post;
+  post: Post & { user: Pick<User, "username" | "name"> };
 };
 
-export enum createPostError {
-  UNKNOWN,
-}
+export enum PostError {
+  NOT_FOUND = "NOT_FOUND",
+  UNAUTHORIZED = "UNAUTHORIZED",
+  BAD_REQUEST = "BAD_REQUEST"
+};
