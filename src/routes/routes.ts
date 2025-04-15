695ef72e7896bb4ef041dab4265f9a6e756e6e4c
AKSHAY K <akshay47kk@gmail.com>
2025-04-15 19:29:58 +0530

    Refactor route handlers for comments, likes, posts, and users to improve error handling and response structure; update import paths for consistency.

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,14 +1,24 @@
 import { Hono } from "hono";
 import { authenticationRoutes } from "./authentication-routes.js";
-import { usersRoutes } from "./users-routes";
-import { postsRoutes } from "./posts-routes";
-import { likesRoutes } from "./likes-routes";
-import { commentsRoutes } from "./comments-routes";
+import { usersRoutes } from "./users-routes.js";
+import { postsRoutes } from "./posts-routes.js";
+import { likesRoutes } from "./likes-routes.js";
+import { commentsRoutes } from "./comments-routes.js";
+import { logger } from "hono/logger";
+import { swaggerRoutes } from "./swagger-routes";
+
 
 export const allRoutes = new Hono();
+
+allRoutes.use(logger());
 
 allRoutes.route("/auth", authenticationRoutes);
 allRoutes.route("/users", usersRoutes);
 allRoutes.route("/posts", postsRoutes);
 allRoutes.route("/likes", likesRoutes);
 allRoutes.route("/comments", commentsRoutes);
+allRoutes.route("/ui", swaggerRoutes);
+
+allRoutes.get("/health", (context) => {
+  return context.json({ message: "All Ok" }, 200);
+});
